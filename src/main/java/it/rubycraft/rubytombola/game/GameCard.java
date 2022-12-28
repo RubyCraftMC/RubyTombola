5ae48cef21e505207b0a132221139b428985a0de
HeroPB <pietrobacigalupi@gmail.com>
2022-12-28 22:42:47 +0100

    Si vola sulla luna 🚀🚀🚀🚀🚀🚀🚀🚀

--- src/main/java/it/rubycraft/rubytombola/game/GameCard.java
+++ src/main/java/it/rubycraft/rubytombola/game/GameCard.java
@@ -2,4 +2,8 @@ package it.rubycraft.rubytombola.game;
 
 public class GameCard {
     private GameNumber[][] numberTable;
+
+    public GameCard(GameNumber[][] numberTable) {
+        this.numberTable = numberTable;
+    }
 }
--- src/main/java/it/rubycraft/rubytombola/utils/Utils.java
+++ src/main/java/it/rubycraft/rubytombola/utils/Utils.java
@@ -2,6 +2,7 @@ package it.rubycraft.rubytombola.utils;
 
 import it.rubycraft.rubytombola.RubyTombola;
 import it.rubycraft.rubytombola.game.GameCard;
+import it.rubycraft.rubytombola.game.GameNumber;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -12,8 +13,11 @@ import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -49,51 +53,67 @@ public class Utils {
     }
 
     public static GameCard giveNewCard(Player player) {
+
+        String[][] matrice = createMatrix();
+        GameNumber[][] gameNumbers = new GameNumber[3][9];
         MapView map = Bukkit.createMap(player.getWorld());
         BufferedImage image = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB);
         Graphics graphics = image.getGraphics();
 
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            List<Integer> rm = new ArrayList<>();
+        // Background bianco
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, 128, 128);
 
-            while (rm.size() < 5) {
-                int randomNumber = new Random().nextInt(10);
-                if (!rm.contains(randomNumber)) {
-                    rm.add(randomNumber);
-                }
-            }
+        // immagine di sfondo
+        Image bg = null;
+        try {
+            bg = ImageIO.read(new URL("https://i.imgur.com/Uw6XUy9.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        graphics.drawImage(bg, 0, 0, 128, 128, null);
 
-            for (int j = 0; j < 9; j++) {
-                graphics.setColor(Color.BLACK);
-                graphics.drawRect(j * 14, i * 14, 14, 14);
-                if (!rm.contains(j)) continue;
-                int number;
-                do {
-                    number = j * 10 + new Random().nextInt(9) + 1;
-                } while (numbers.contains(number));
-                numbers.add(number);
+        // righe
+        for (int y = 0; y < 3; y++) {
+
+            // colonne
+            for (int x = 0; x < 9; x++) {
+                //Creazione della scheda di base ( i quadrati)
                 graphics.setColor(Color.WHITE);
+                graphics.fillRect(x * 14, 45 + y * 14, 14, 14);
+                graphics.setColor(Color.BLACK);
+                graphics.drawRect(x * 14, 45 + y * 14, 14, 14);
+
+                if (matrice[x][y].equalsIgnoreCase("empty")) {
+                    gameNumbers[x][y] = GameNumber.empty();
+                    continue;
+                }
+
+                // Scrive i numeri
+                graphics.setColor(Color.BLACK);
                 graphics.setFont(new Font("Arial", Font.PLAIN, 10));
-                graphics.drawString(Integer.toString(number), j * 14 + 2, i * 14 + 12);
-                graphics.setColor(Color.RED);
-                graphics.setFont(new Font("Arial", Font.PLAIN, 15));
-                graphics.drawString("X", j * 14 + 2, i * 14 + 13);
+                graphics.drawString(matrice[x][y], x * 14 + 2, 45 + y * 14 + 12);
+                gameNumbers[x][y] = new GameNumber(Integer.parseInt(matrice[x][y]), false);
             }
         }
+
+        //Crea il render custom
         MapRenderer renderer = new MapRenderer() {
             @Override
             public void render(MapView map, MapCanvas canvas, Player player) {
-                canvas.drawImage(0, 40, image);
+                canvas.drawImage(0, 0, image);
             }
         };
+
+        //Rimuove i render e aggiunge quello custom
         map.getRenderers().forEach(map::removeRenderer);
         map.addRenderer(renderer);
+        //Givva l'item al player
         int mapId = map.getId();
         ItemStack mapItem = new ItemStack(Material.MAP, 1, (short) mapId);
         player.getInventory().addItem(mapItem);
 
-        return card;
+        return new GameCard(gameNumbers);
     }
 
     public static void update(Player player, ItemStack itemStack, String[][] cardMatrix, Predicate<String> filter) {
@@ -101,22 +121,42 @@ public class Utils {
         BufferedImage image = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB);
         Graphics graphics = image.getGraphics();
 
+        // Background bianco
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, 128, 128);
+
+        // immagine di sfondo
+        Image bg = null;
+        try {
+            bg = ImageIO.read(new URL("https://i.imgur.com/Uw6XUy9.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        graphics.drawImage(bg, 0, 0, 128, 128, null);
+
+        // righe
         for (int y = 0; y < 3; y++) {
+
+            // colonne
             for (int x = 0; x < 9; x++) {
+                //Creazione della scheda di base ( i quadrati)
+                graphics.setColor(Color.WHITE);
+                graphics.fillRect(x * 14, 45 + y * 14, 14, 14);
                 graphics.setColor(Color.BLACK);
-                graphics.drawRect(y * 14, y * 14, 14, 14);
+                graphics.drawRect(x * 14, 45 + y * 14, 14, 14);
+
                 if (cardMatrix[x][y].equalsIgnoreCase("empty")) {
                     continue;
                 }
 
-                graphics.setColor(Color.WHITE);
+                // Scrive i numeri
+                graphics.setColor(Color.BLACK);
                 graphics.setFont(new Font("Arial", Font.PLAIN, 10));
-                graphics.drawString(cardMatrix[x][y], x * 14 + 2, y * 14 + 12);
-
-                if (filter.test(cardMatrix[x][y])) {
+                graphics.drawString(cardMatrix[x][y], x * 14 + 2, 45 + y * 14 + 12);
+                if(filter.test(cardMatrix[x][y])) {
                     graphics.setColor(Color.RED);
-                    graphics.setFont(new Font("Arial", Font.PLAIN, 15));
-                    graphics.drawString("X", x * 14 + 2, y * 14 + 13);
+                    graphics.drawLine(x * 14 + 2, 45 + y * 14 + 2, (x + 1) * 14 - 2, (y + 1) * 14 + 45 - 2);
+                    graphics.drawLine((x + 1) * 14 - 2, 45 + y * 14 + 2, x * 14 + 2, (y + 1) * 14 + 45 - 2);
                 }
             }
         }
